53f42d536a1311d8501f4dbe43f81d2199504993
nikita <niki2ta00n4@gmail.com>
2026-01-19 17:44:00 +0300

    feat: add list and card for BlogPage

--- src/components/shared/blog-block/blog-block.tsx
+++ src/components/shared/blog-block/blog-block.tsx
@@ -1,15 +1,99 @@
+'use client';
+import { useMemo, useState } from 'react';
+
+import { cn } from '@/lib/utils';
+
+import { Media } from '@/components/ui/media';
 import Section from '@/components/ui/section/section';
 
+import BlogCard from './blog-card';
+import BlogCardMobile from './blog-card-mobile';
 import { BLOG_CONFIG } from './blog-config';
+// TODO: подправить Hero assets
+// TODO: сделать пагинацию, сделать карточки адаптивными, сделать так чтобы карточки не выходили за пределы контейнера
+// TODO: написать Серёге
+const LIST_SIZE = 9;
 
 export default function BlogBlock() {
+  const [list, setList] = useState(0);
+  const totalLists = Math.max(1, Math.ceil(BLOG_CONFIG.length / LIST_SIZE));
+  const canPrev = list > 0;
+  const canNext = list < totalLists - 1;
+
+  const data = useMemo(() => {
+    const start = list * LIST_SIZE;
+    const end = start + LIST_SIZE;
+    return BLOG_CONFIG.slice(start, end);
+  }, [list]);
+
+  const goToList = (nextList: number) => {
+    const clamped = Math.min(Math.max(nextList, 0), totalLists - 1);
+    setList(clamped);
+  };
+
   return (
-    <Section>
-      <ul>
-        {BLOG_CONFIG.map((item) => (
-          <li key={item.id}>{item.title}</li>
-        ))}
+    <Section className="gap-[32px]">
+      <ul className="grid grid-cols-1 gap-[clamp(14px,2vw,20px)] sm:grid-cols-3 2xl:grid-cols-4">
+        {data.map((card, index) => {
+          return index === 0 ? (
+            <BlogCardMobile card={card} key={card.id} />
+          ) : (
+            <BlogCard card={card} key={card.id} />
+          );
+        })}
       </ul>
+      <div className="flex w-full items-center justify-between">
+        <div className="flex">
+          {Array.from({ length: Math.ceil(BLOG_CONFIG.length / LIST_SIZE) }, (_, i) => i).map(
+            (item, index) => {
+              const isActiveList = list === index;
+              return (
+                <button
+                  className={cn(
+                    'flex-center font-second-family h-[clamp(32px,3vw,42px)] w-[clamp(32px,3vw,42px)] rounded-full text-[16px]',
+                    isActiveList ? 'bg-primary text-white' : 'text-foreground3 bg-transparent'
+                  )}
+                  key={item}
+                  onClick={() => goToList(index)}
+                  type="button"
+                >
+                  {item + 1}
+                </button>
+              );
+            }
+          )}
+        </div>
+        <div className="flex">
+          <button
+            className={cn(
+              'flex-center h-[42px] w-[42px] rounded-full border border-[#dce2fe] bg-white transition-all',
+              canPrev ? 'hover:opacity-80' : 'cursor-not-allowed opacity-40'
+            )}
+            disabled={!canPrev}
+            onClick={() => goToList(list - 1)}
+            type="button"
+          >
+            <Media
+              className="flex-center h-[24px] w-[12px]"
+              image={{ src: '/arrow/arrow-left-without-stick.svg', alt: 'arrow-left' }}
+            />
+          </button>
+          <button
+            className={cn(
+              'flex-center h-[42px] w-[42px] rounded-full border border-[#dce2fe] bg-white transition-all',
+              canNext ? 'hover:opacity-80' : 'cursor-not-allowed opacity-40'
+            )}
+            disabled={!canNext}
+            onClick={() => goToList(list + 1)}
+            type="button"
+          >
+            <Media
+              className="flex-center h-[24px] w-[12px]"
+              image={{ src: '/arrow/arrow-right-without-stick.svg', alt: 'arrow-right' }}
+            />
+          </button>
+        </div>
+      </div>
     </Section>
   );
 }
---
+++ src/components/shared/blog-block/blog-card-mobile.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+
+import { Media } from '@/components/ui/media';
+import Typography from '@/components/ui/typography/typography';
+
+import { BlogCard as TBlogCard } from './blog-config';
+
+export default function BlogCardMobile({ card }: { card: TBlogCard }) {
+  return (
+    <div className="z-10 flex h-[clamp(293px,28vw,380px)] w-[clamp(332px,30vw,435px)] flex-col justify-between rounded-[24px] bg-white p-[14px] sm:p-[24px]">
+      {card.image && (
+        <Media
+          className="h-[clamp(129,20vw,165px)] w-[clamp(304px,55vw,387px)]"
+          image={{ src: card.image.src, alt: card.image.alt }}
+        />
+      )}
+      <div className="flex max-h-[55px] flex-nowrap items-center justify-between gap-[10px]">
+        <Typography className="lowercase" variant="h3">
+          {card.title}
+        </Typography>
+        <Link
+          className="flex-center bg-primary h-[clamp(32px,3vw,42px)] w-[clamp(32px,3vw,42px)] shrink-0 rounded-full"
+          href={card.href || '/'}
+        >
+          <Media
+            className="h-[24px] w-[24px]"
+            image={{ src: '/arrow/arrow-right-white-mobile.svg', alt: 'arrow' }}
+          />
+        </Link>
+      </div>
+
+      <Typography className="leading-[140%] opacity-80" variant="p2">
+        {card.description}
+      </Typography>
+    </div>
+  );
+}
