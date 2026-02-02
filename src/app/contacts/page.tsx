48d91e8711304656f877ae64a86c589c88575f99
niki_tusa <niki2ta00n4@gmail.com>
2026-02-02 06:53:57 +0300

    feat: add seo

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,7 +1,22 @@
+import { Metadata } from 'next';
+
 import ContactsBlock from '@/components/shared/contacts-block/contacts-block';
 import HeroBlock from '@/components/shared/hero-block/hero-block';
 
-export default function CasesPage() {
+import { SITE_IMAGES } from '@/constants/seo-constants';
+
+export const metadata: Metadata = {
+  title: 'Контакты',
+
+  description: 'Наши контакты',
+  openGraph: {
+    title: 'Контакты',
+    description: 'Наши контакты',
+    images: [{ url: SITE_IMAGES, width: 1200, height: 630, alt: 'ITWEBS' }],
+  },
+  robots: { index: true, follow: true },
+};
+export default function ContactsPage() {
   return (
     <>
       <HeroBlock heading="Наши контакты" />
