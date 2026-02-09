8b16fadd5dff24c472406a0acbee9f701c9b1c18
nikita <niki2ta00n4@gmail.com>
2026-02-09 17:03:42 +0300

    refactor: replace text on text-blocks

--- src/components/shared/about-text-block/about-text-block.tsx
+++ src/components/shared/about-text-block/about-text-block.tsx
@@ -5,13 +5,14 @@ export default function AboutTextBlock() {
   return (
     <Section>
       <Typography className="max-w-[1497px]" variant="p-big">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-        galley of type and scrambled it to make a type
-        <b className="text-foreground2/50">
-          specimen book. It has survived not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged.
-        </b>
+        — Компания ITWEBS уже более пяти лет является надежным технологическим партнером для
+        бизнеса. За годы работы мы накопили экспертизу в создании эффективных digital-решений: от
+        профессиональной <b className="text-foreground2/50">разработки сайта</b> и мощного
+        <b className="text-foreground2/50">SEO продвижения</b> до комплексной
+        <b className="text-foreground2/50">IT поддержки бизнеса</b>. Наш подход, доказавший
+        результативность в различных сферах, позволяет предлагать клиентам только проверенные
+        решения. Для вашего роста — свяжитесь с ITWEBS, чтобы
+        <b className="text-foreground2/50">заказать мобильное приложение</b> или другое IT-решение.
       </Typography>
     </Section>
   );
